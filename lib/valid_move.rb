cf53f5e1b3e1c38a61d4aa8a5c6c33a5fbdd7918
Phoebe Lau <phoebelau7@gmail.com>
2017-11-15 03:14:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
-def valid_move?(board, index)
-if position_taken?
-  puts "Please choose a valid move"
-else
-  board[index] = "X"
+def valid_move?
+  if position_taken? 
+false
+  else 
+    true
 end
 end
 
