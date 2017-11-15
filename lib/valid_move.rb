2ae2da74766910f5a8099b43475c686aaccd2d08
Phoebe Lau <phoebelau7@gmail.com>
2017-11-15 03:07:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,6 +5,7 @@ if position_taken? = true
 elsif position_taken? = false
   true
 end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
