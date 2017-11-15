47c7768c2f1b254865efc9a022abb1e861bd79b7
Phoebe Lau <phoebelau7@gmail.com>
2017-11-15 03:24:37 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
-def valid_move?(user_input)
-  if user_input > 9 or user_input < 1
-    nil
-  elsif
-    position_taken?
+def valid_move?(board,index)
+  if position_taken?
+    true
+  else
+    false
   end
 end
 
